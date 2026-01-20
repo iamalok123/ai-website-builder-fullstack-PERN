69eda821a2f6a9d8dd6fc2a7911ccbc2405c42e3
Alok <alokhotta10@gmail.com>
2026-01-21 00:23:59 +0530

    Impliment Backend API's for different projectRoutes

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 import userRouter from './routes/userRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 app.use('/api/user', userRouter);
+app.use('/api/project', projectRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
