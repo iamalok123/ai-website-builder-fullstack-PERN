a60258616aad8848030afcdd3399df5469c4b75c
Alok <alokhotta10@gmail.com>
2026-01-20 20:34:58 +0530

    Impliment API's for different userRoutes

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import 'dotenv/config' ;
+import 'dotenv/config';
 import cors from 'cors';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -11,15 +12,19 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
     credentials: true,
-} 
+}
 
 app.use(cors(corsOptions));
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+app.use(express.json({limit: '50mb'}));
+
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+app.use('/api/user', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
